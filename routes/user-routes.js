5a61c9781b6b9982fa4e22a36ed19a4371d0b103
Madhumitha <madhumithavenu03@gmail.com>
2023-12-11 11:17:14 +0530

    adding function to login page

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const {signup} = require("../controllers/user-controller.js");
+const {signup,login} = require("../controllers/user-controller.js");
 
 
 const userRouter = express.Router();
-
-
 userRouter.post("/signup",signup);
-
+userRouter.post("/login",login);
 
 module.exports = userRouter;
